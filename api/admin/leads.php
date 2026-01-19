cfad27f40c5138cffbe9d1f0c44dd541632a4df7
Yogii <yogii@Yogiis-MacBook-Pro.local>
2026-01-19 15:23:24 +0530

    Fix CORS headers for finonest.com domain

--- api/admin/leads.php
+++ api/admin/leads.php
@@ -1,7 +1,8 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://finonest.com');
 header('Access-Control-Allow-Methods: GET, PUT, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
--- api/leads.php
+++ api/leads.php
@@ -1,7 +1,8 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://finonest.com');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, X-API-Key');
+header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
