ef78593d6c2a9209389b4e7adcd687090296a695
Yogii <yogii@Yogiis-MacBook-Pro.local>
2026-01-22 12:28:22 +0530

    Fix CORS headers in upload-image API

--- api/upload-image.php
+++ api/upload-image.php
@@ -1,12 +1,16 @@
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: https://finonest.com');
+    header('Access-Control-Allow-Methods: POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
     exit(0);
 }
+
+header('Access-Control-Allow-Origin: https://finonest.com');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../config/jwt.php';
