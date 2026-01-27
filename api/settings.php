a0f6619b52d4b6e5189b7a483af99c771e7f9489
Yogii <yogii@Yogiis-MacBook-Pro.local>
2026-01-27 14:38:34 +0530

    Add new credential fields to settings API

--- api/settings.php
+++ api/settings.php
@@ -82,6 +82,11 @@ try {
         ['credit_secret_key', '', 'Credit API secret key'],
         ['credit_access_key', '', 'Credit API access key'],
         ['credit_service_id', '', 'Credit API service ID'],
+        ['client_hash_id', '', 'Client Hash ID for API authentication'],
+        ['base_url', '', 'Base URL for API calls'],
+        ['secret_key', '', 'Secret key for API authentication'],
+        ['access_key', '', 'Access key for API authentication'],
+        ['service_id', '', 'Service ID for Credit API'],
         ['surepass_api_url', 'https://kyc-api.surepass.io/api/v1/rc/rc-full', 'SurePass RC API URL'],
         ['surepass_token', '', 'SurePass API token']
     ];
@@ -270,7 +275,11 @@ function getSettingDescription($key) {
         'credit_access_key' => 'Credit API access key',
         'credit_service_id' => 'Credit API service ID',
         'surepass_api_url' => 'SurePass RC API URL',
-        'surepass_token' => 'SurePass API token'
+        'client_hash_id' => 'Client Hash ID for API authentication',
+        'base_url' => 'Base URL for API calls',
+        'secret_key' => 'Secret key for API authentication',
+        'access_key' => 'Access key for API authentication',
+        'service_id' => 'Service ID for Credit API',
     ];
     return $descriptions[$key] ?? '';
 }
