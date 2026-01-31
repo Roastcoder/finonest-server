35caf551e6873cfc3503e576c91270265f021f05
Yogii <yogii@Yogiis-MacBook-Pro.local>
2026-01-31 12:38:23 +0530

    Fix CORS issues in DSA APIs by using standard cors-handler.php

--- api/dsa-registration.php
+++ api/dsa-registration.php
@@ -1,15 +1,5 @@
 <?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Max-Age: 86400');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit(0);
-}
-
+require_once __DIR__ . '/../cors-handler.php';
 require_once '../config/database.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
